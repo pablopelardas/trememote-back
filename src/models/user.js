824d4af794d220a819f57102e4b0db289198c742
pablo pelardas <pablo.pelardas@gmail.com>
2022-08-01 21:02:04 -0300

    feature: add models

---
+++ src/models/user.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+        static associate(models) {
+            // define association here
+            User.hasMany(models.Post, { foreignKey: 'user_id'});
+        }
+    }
+    User.init({
+        username: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        refreshToken: DataTypes.STRING,
+    }, {
+        sequelize,
+        paranoid: true,
+        modelName: 'User',
+    });
+    return User;
+};
